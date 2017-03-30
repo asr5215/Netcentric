93ccccb2de40f3f4e6082e8c7011e3dd06c74584
marshmellomoose <marshmellomoose@gmail.com>
2017-03-30 18:59:07 -0400

    will run now

--- asg4/webServer.js
+++ asg4/webServer.js
@@ -1,17 +1,38 @@
-var http = require("http");
+var http = require('http'), qs = require('querystring');
 
-function process_request(req, res){
-	//shows incoming request
-	console.log("INCOMING REQUEST: " + req.method + " " + req.url);
+function handle_incoming_request(req, res){
+	//get post data for request
+	var body = ' ';
+	req.on(
+		'readable', 
+		function (){
+			var d = req.read();
+			if(d){
+				if(typeof d == 'string'){
+					body += d;
+				} else if(typeof d == 'object' && d instanceof Buffer){
+					body += d.toString('utf8');
+				}
+			}
+		}
+	);
 	
-	var body = 'Thanks for providing your feedback! \n';
-	var content_length = body.length;
-	res.writeHead(200, {
-		'Content-Length': content_length,
-		'Content-Type': 'text/plain'
-	});
-	res.end(body);	
+	//process post data
+	req.on(
+		'end', 
+		function (){
+			if(req.method.toLowerCase() == 'post'){
+				//use querystring module parse function to represent the data sent as js object
+				var POST_data = qs.parse(body);
+				//code to display object as text in command window
+				console.log(POST_data);
+			}
+			res.writeHead(200, {'Content-Type':'text/plain'});
+			res.end(JSON.stringify(POST_data));
+		}
+	);
 }
+				
 
-var s = http.createServer(process-request);
+var s = http.createServer(handle_incoming_request);
 s.listen(8080);
